9a812fd82ad71dc8b7aa60f2f3f07f97702d9662
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-08 05:23:43 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,7 @@ def bonus
  }
 }
 
-bonus[:montague][:hero][:status] = "dead"
-bonus[:capulet][:heroine][:status] = "dead"
-puts bonus
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+puts epic_tragedy
 end
